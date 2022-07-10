c0549b0e9fcefea9871f86d1fb092080ac69c9e6
Stan <skukhniy@gmail.com>
2022-07-10 00:29:16 -0400

    cleaned up returnCord func

--- src/components/Game/GameRender.js
+++ src/components/Game/GameRender.js
@@ -3,24 +3,26 @@ import './GameRender.scss'
 import gamecube from "../../assets/gamecube.jpeg"
 
 export default function GameRender() {
+  // this function will grab the current image data and divide it by the 
+  // original image data to find the accurate cordinates of a character
+  // no matter the screen size
   function returnCordinates(e){
     console.log(e)
+    // gets the current click cordinates in relation to the image
     const offsetX = e.nativeEvent.offsetX
     const offsetY = e.nativeEvent.offsetY
+    // this is the size of the image in the HTML
     const offsetWidth = e.target.offsetWidth
     const offsetHeight = e.target.offsetHeight
+    // the original resolution of the image
     const originalWidth = 1080
     const originalHeight= 1842
-    const growthFactorY = offsetHeight/originalHeight
+    // determines how much the image grew once it was added to the HTML
+    const growthFactorY = offsetHeight/originalHeight 
     const growthFactorX = offsetWidth/originalWidth
-    const targetX = offsetX/growthFactorX
+    // reverses the growth to find what the cordinates would be at the original image resolution
+    const targetX = offsetX/growthFactorX 
     const targetY = offsetY/growthFactorY
-    console.log(offsetX)
-    console.log(offsetY)
-    console.log(offsetWidth)
-    console.log(offsetHeight)
-    console.log(offsetHeight/originalHeight)
-    console.log(offsetHeight/growthFactorY)
     console.log(targetX)
     console.log(targetY)
   }
