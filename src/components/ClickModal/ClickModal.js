11eca67fabd65ac799ec5ae5f3e8b07189991778
Stan <skukhniy@gmail.com>
2022-07-10 20:31:57 -0400

    added base css for click modal

--- src/components/ClickModal/ClickModal.js
+++ src/components/ClickModal/ClickModal.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import './ClickModal.scss';
+import Luigi from '../../assets/Luigi.png';
+import Link from '../../assets/Link.png';
+import Toad from '../../assets/Toad.png';
 
 export default function ClickModal() {
   return (
-    <div className="ModalContainer">ClickModal</div>
+    <div className="ModalContainer">
+      <div className="charButton">
+        <button type="button">
+          <img src={Luigi} alt="Luigi" />
+          <span>Luigi</span>
+        </button>
+      </div>
+      <div className="charButton">
+        <button type="button">
+          <img src={Toad} alt="Toad" />
+          <span>Toad</span>
+        </button>
+      </div>
+      <div className="charButton">
+        <button type="button">
+          <img src={Link} alt="Link" id="linkImg" />
+          <span>Link</span>
+        </button>
+      </div>
+    </div>
   );
 }
