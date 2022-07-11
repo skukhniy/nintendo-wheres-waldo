7445b6c5385b7b8a02d5aff0cc7d03fa040bfc3e
Stan <skukhniy@gmail.com>
2022-07-10 21:41:28 -0400

    added cordinates for the click modal to move depending on where you clicked

--- src/components/ClickModal/ClickModal.js
+++ src/components/ClickModal/ClickModal.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './ClickModal.scss';
 import Luigi from '../../assets/Luigi.png';
 import Link from '../../assets/Link.png';
 import Toad from '../../assets/Toad.png';
 
-export default function ClickModal() {
+export default function ClickModal({ cordinates }) {
+  ClickModal.propTypes = {
+    cordinates: PropTypes.objectOf.isRequired,
+  };
+  function grabModalCordinates() {
+    const modalX = cordinates.clickedX + 20;
+    const modalY = cordinates.clickedY - 20;
+    return ({ x: modalX, y: modalY });
+  }
+  const modalCordinates = grabModalCordinates();
+  console.log(cordinates);
   return (
-    <div className="ModalContainer">
+    <div className="ModalContainer" style={{ top: `${modalCordinates.y}px`, left: `${modalCordinates.x}px` }}>
       <div className="charButton">
         <button type="button">
           <img src={Luigi} alt="Luigi" />
--- src/components/Game/GameRender.js
+++ src/components/Game/GameRender.js
@@ -9,38 +9,48 @@ import ClickModal from '../ClickModal/ClickModal';
 
 export default function GameRender() {
   const [clickModalState, setClickModal] = useState(false);
+  const [cordinates, setCordinates] = useState({});
 
   // const luigiRef = doc(db, 'Cordinates', 'Luigi');
   // getDoc(luigiRef)
   //   .then((document) => {
   //     console.log(document.data());
   //   });
-  // function returnCordinates(e) {
-  //   // gets the current click cordinates in relation to the image
-  //   const { offsetX } = e.nativeEvent;
-  //   const { offsetY } = e.nativeEvent;
-  //   // this is the size of the image in the HTML
-  //   const { offsetWidth } = e.target;
-  //   const { offsetHeight } = e.target;
-  //   // the original resolution of the image
-  //   const originalWidth = 1080;
-  //   const originalHeight = 1842;
-  //   // determines how much the image grew once it was added to the HTML
-  //   const growthFactorY = offsetHeight / originalHeight;
-  //   const growthFactorX = offsetWidth / originalWidth;
-  //   // reverses the growth to find what the cordinates would be at the original image resolution
-  //   const targetX = offsetX / growthFactorX;
-  //   const targetY = offsetY / growthFactorY;
-  //   console.log(`X: ${targetX}`);
-  //   console.log(`Y: ${targetY}`);
-  //   return { xCord: targetX, yCord: targetY };
-  // }
-  // // 1842
-  // // 1080
+  function returnCordinates(e) {
+    // gets the current click cordinates in relation to the image
+    const { offsetX } = e.nativeEvent;
+    const { offsetY } = e.nativeEvent;
+    // this is the size of the image in the HTML
+    const { offsetWidth } = e.target;
+    const { offsetHeight } = e.target;
+    // the original resolution of the image
+    const originalWidth = 1080;
+    const originalHeight = 1842;
+    // determines how much the image grew once it was added to the HTML
+    const growthFactorY = offsetHeight / originalHeight;
+    const growthFactorX = offsetWidth / originalWidth;
+    // reverses the growth to find what the cordinates would be at the original image resolution
+    const targX = offsetX / growthFactorX;
+    const targY = offsetY / growthFactorY;
+    console.log(`X: ${targX}`);
+    console.log(`Y: ${targY}`);
+    return {
+      clickedX: offsetX, clickedY: offsetY, targetX: targX, targetY: targY,
+    };
+  }
+  // 1842
+  // 1080
   return (
     <div className="GameContainer">
-      <img src={gamecube} alt="gamecube" onClick={() => setClickModal(!clickModalState)} />
-      {clickModalState && <ClickModal />}
+      <img
+        src={gamecube}
+        alt="gamecube"
+        onClick={(e) => {
+          setClickModal(!clickModalState);
+          setCordinates(returnCordinates(e));
+        }}
+      />
+      {clickModalState && <ClickModal cordinates={cordinates} />}
     </div>
   );
 }
